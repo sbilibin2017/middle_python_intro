76472df4e2c8909dd1795829f534f616577bcec2
Sergey Bilibin <s.bilibin2017@yandex.ru>
2023-01-21 08:47:34 +0300

    Исправлена функция greeting в main.py

--- app/main.py
+++ app/main.py
@@ -7,6 +7,10 @@ def greeting(name: str) -> str:
     :param str name: имя пользователя
     :return str: текст приветствия
     '''
-    name_prepared = ' '.join([word.capitalize() for word in name.split(' ')])
-
+    # получаем имя и фамилию
+    name_split = name.split(' ')
+    # заглавные буквы
+    name_split_capitalized = [word.capitalize() for word in name_split]
+    # склеиваем полное имя
+    name_prepared = ' '.join(name_split_capitalized)
     return f'Привет, {name_prepared}'
